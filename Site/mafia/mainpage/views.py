2e2de2f527cc66446062fb970b63af50dd4266e1
Orlov Gleb <Excep404@gmail.com>
2023-05-13 12:52:24 +0300

    Отделил content от mainpage

--- Site/mafia/mainpage/views.py
+++ Site/mafia/mainpage/views.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 
 
 def index(request):
-    return render(request, 'mainpage/index.html')
+    return render(request, 'mainpage/mainpage.html')
 
 
 def pageNotFound(request, exception):
