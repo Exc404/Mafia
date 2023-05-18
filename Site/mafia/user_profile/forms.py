f66cc189546e0f158ded753b019107100be8e78e
Pashka <pasha2003vasenin@gmail.com>
2023-05-18 11:24:44 +0300

    Страница редактирования профия и форма для этого

--- Site/mafia/user_profile/forms.py
+++ Site/mafia/user_profile/forms.py
@@ -3,17 +3,19 @@ from .models import Profile
 from django import forms
 
 
-class ImageForm(forms.ModelForm):
-    profileImg = forms.ImageField
+class EditProfileForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    nickname = forms.CharField(label="Новый никнейм", max_length=100)
+    profile_img = forms.ImageField(label="Новое фото профиля:", required=False)
 
     class Meta:
         model = Profile
-        fields = ('profile_img',)
+        fields = ('profile_img', 'nickname')
 
 
-class NicknameForm(forms.ModelForm):
-    nickname = forms.ImageField
 
-    class Meta:
-        model = Profile
-        fields = ('nickname',)
+
+
