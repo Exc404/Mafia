ce15364c118ea439989c399c7139e103a69132df
zZLerisZz <yalerisatinet@gmail.com>
2023-06-02 21:26:45 +0300

    ЕБЛЯ С ТАРАКАНАМИ

--- Site/mafia/lobbypage/static/js/streams.js
+++ Site/mafia/lobbypage/static/js/streams.js
@@ -25,7 +25,7 @@ inputForm.addEventListener('submit', (e) => {
 window.onload = function () {
     let startButton = document.getElementById('startgame')
     startButton.onclick = function () {
-        if (is_host) alert("Вы начали игру")
+        if (is_host) alert("Вы запустили игру")
         else alert("Вы не являетесь создателем комнаты")
         return false
     }
@@ -42,7 +42,8 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
-    console.log("Начался шпионаж за твоей жопой!")
+    client.on('user-published', handleUserJoined)
+    alert("Начался шпионаж за твоей жопой!")
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
@@ -53,6 +54,28 @@ let joinAndDisplayLocalStream = async () => {
     document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
     localTracks[1].play(`user-${UID}`)
     await client.publish([localTracks[0], localTracks[1]])
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.uid] = user
+    console.log("AJSNCOANJS")
+    console.log(mediaType)
+    await client.subscribe(user, mediaType)
+    if(mediaType === "video") {
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if(player != null){
+            player.remove()
+        }
+        player = `<div class="video-container" id = "user-container-${user.uid}">
+                    <div class="user-name-wrapper"><span class="user-name">Имя пользователя</span></div>
+                    <div class="video-player" id = "user-${user.uid}"></div>
+                </div>`
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if(mediaType === "audio") {
+        user.audioTrack.play()
+    }
 }
 
 joinAndDisplayLocalStream()
