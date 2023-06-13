273857d647a97a512623b6c8525e2882ce7dfb28
Orlov Gleb <Excep404@gmail.com>
2023-06-13 21:54:50 +0300

    ВидетЪ УведомитЪ БытЪ

--- Site/mafia/mainpage/views.py
+++ Site/mafia/mainpage/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.template.loader import render_to_string
-
+from user_profile.models import Notice
 
 def index(request):
-    return render(request, 'mainpage/mainpage.html', {'request': request})
+    data = {}
+    if request.user.is_authenticated:
+        data['NoticesCount'] = Notice.objects.filter(addressee=request.user.profile).count()
+    data['request'] = request
+    return render(request, 'mainpage/mainpage.html', data)
 
 def faqpage(request):
     return render(request, 'mainpage/faqpage.html', {'request':request})
